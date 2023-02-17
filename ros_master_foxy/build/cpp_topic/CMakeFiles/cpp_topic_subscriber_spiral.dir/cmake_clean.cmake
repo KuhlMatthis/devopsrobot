eeb35abed7f8f9c000c7d00057cc275eab90e325
noebernigaud <noe.bernigaud@etu.unice.fr>
2023-02-17 13:59:08 +0100

    ros project intégré

---
+++ ros_master_foxy/build/cpp_topic/CMakeFiles/cpp_topic_subscriber_spiral.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_topic_subscriber_spiral.dir/src/cpp_topic_subscriber_spiral.cpp.o"
+  "cpp_topic_subscriber_spiral"
+  "cpp_topic_subscriber_spiral.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_topic_subscriber_spiral.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ros_master_foxy/build/cpp_topic/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/agigas/devopsrobot/ros_master_foxy/ros2_basics/cpp_topic
+# Build directory: /home/agigas/devopsrobot/ros_master_foxy/build/cpp_topic
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
