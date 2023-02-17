01c4cec7d44036fef89b453fc096566a06f29175
= <matthis.kuhl@etu.univ-cotedazur.fr>
2023-02-17 13:17:50 +0100

    update

--- ros_master_foxy/build/cpp_topic/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/agigas/devopsrobot/ros_master_foxy/ros2_basics/cpp_topic
-# Build directory: /home/agigas/devopsrobot/ros_master_foxy/build/cpp_topic
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
