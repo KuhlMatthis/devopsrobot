c1d8a120537e0fd068d082d890eba91f790964ea
noebernigaud <noe.bernigaud@etu.unice.fr>
2023-02-22 16:27:11 +0100

    gazebo correctly closed when closing script

--- ros_master_foxy/simulationtest.sh
+++ ros_master_foxy/simulationtest.sh
@@ -1,3 +1,10 @@
-#! /bin/bash
-/bin/bash -c 'ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py &'
+#!/bin/bash
+source /opt/ros/foxy/setup.bash
+export TURTLEBOT3_MODEL=burger
+export ROS_DOMAIN_ID=30
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+/bin/bash -c 'source /opt/ros/foxy/setup.bash && export TURTLEBOT3_MODEL=burger && export ROS_DOMAIN_ID=30 && ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py &'
 /bin/bash -c 'sudo docker build --build-arg base="ros:foxy" -t turtlebotfoxy . && sudo docker run -it turtlebotfoxy'
+
