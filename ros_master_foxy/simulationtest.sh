55ee4b76d5f29df2e22c03dec8ac865daa1f31b9
noebernigaud <noe.bernigaud@etu.unice.fr>
2023-02-22 16:36:46 +0100

    cleaning up script

--- ros_master_foxy/simulationtest.sh
+++ ros_master_foxy/simulationtest.sh
@@ -5,6 +5,6 @@ export ROS_DOMAIN_ID=30
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-/bin/bash -c 'source /opt/ros/foxy/setup.bash && export TURTLEBOT3_MODEL=burger && export ROS_DOMAIN_ID=30 && ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py &'
-/bin/bash -c 'sudo docker build --build-arg base="ros:foxy" -t turtlebotfoxy . && sudo docker run -it turtlebotfoxy'
+ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py &
+sudo docker build --build-arg base="ros:foxy" -t turtlebotfoxy . && sudo docker run -it turtlebotfoxy
 
